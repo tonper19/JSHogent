43c27d996ac73f47ddc15b7d8bdb5e0d88c17d49
Miguel Perez <miguel.p.donate@gmail.com>
2024-02-26 09:43:39 +0100

    progress

---
+++ randomFractions.js
@@ -0,0 +1,17 @@
+function randomFraction(){
+
+
+    return Math.random();
+}
+
+console.log(randomFraction());
+
+let randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum(){
+
+
+    return Math.florr(Math.random() * 10);
+}
+
+console.log(randomWholeNum());
